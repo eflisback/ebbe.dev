d8aab5d66e344ef71101cb2fa6cfd64274bd69a7
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2023-07-13 21:27:01 +0200

    eslint config ignores files, yay!

--- src/Pages/GptClient/ChatFlow/ChatFlow.tsx
+++ src/Pages/GptClient/ChatFlow/ChatFlow.tsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import styles from "./ChatFlow.module.css";
+import { OpenAIApi } from "openai";
+import { Configuration } from "openai/dist/configuration";
+let openai;
+const instructions = ``;
 
+const api_key = import.meta.env.VITE_OPENAI_API_KEY as string;
+
+// const apiKey = import.meta.env.VITE_OPENAI_API_KEY;
+console.log("ollebolle: ", import.meta.env.VITE_OPENAI_API_KEY);
 class Message {
   public sender: string;
   public content: string;
@@ -13,17 +21,41 @@ class Message {
   }
 }
 
-interface IProps {
-  messages: Message[];
-}
+const testData: Message[] = [
+  new Message("user", "Hello! How are you?"),
+  new Message("model", "I'm doing well, thank you! How about you?"),
+  new Message("user", "I'm good too. Just working on a project."),
+  new Message("model", "That sounds interesting. What kind of project is it?"),
+  new Message(
+    "user",
+    "It's a web development project using the latest frameworks."
+  ),
+  new Message("model", "That's great! Which frameworks are you using?"),
+  new Message(
+    "user",
+    "I'm using React.js for the frontend and Node.js for the backend."
+  ),
+  new Message(
+    "model",
+    "Excellent choice! Both React.js and Node.js are popular and powerful tools."
+  ),
+  new Message(
+    "user",
+    "Thank you! I'm excited about how the project is shaping up."
+  ),
+  new Message(
+    "model",
+    "I'm sure it will turn out great. Best of luck with your development!"
+  ),
+];
 
-export default function ChatFlow(p: IProps) {
+export default function ChatFlow() {
   const [inputValue, setInputValue] = useState("");
 
+  const messages = testData;
+
   const handleMessageSend = () => {
-    console.log("Heya sending message"); // Function to be called when a message is sent
-    // Add your logic for sending the message to the AI model
-    // For now, we're simply logging a message
+    console.log("Heya sending message");
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -33,7 +65,7 @@ export default function ChatFlow(p: IProps) {
   return (
     <div className={styles.back}>
       <div className={styles.messageFlow}>
-        {p.messages.map((message, index) => (
+        {messages.map((message, index) => (
           <div
             className={`${styles.message} ${
               index % 2 === 0 ? styles.brighter : ""
--- src/Pages/GptClient/GptClient.tsx
+++ src/Pages/GptClient/GptClient.tsx
@@ -6,53 +6,13 @@ import Chats from "./Chats/Chats";
 import ChatFlow from "./ChatFlow/ChatFlow";
 import Settings from "./Settings/Settings";
 
-class Message {
-  public sender: string;
-  public content: string;
-  public timestamp: Date;
-
-  constructor(sender: string, content: string) {
-    this.sender = sender;
-    this.content = content;
-    this.timestamp = new Date();
-  }
-}
-
-const testData: Message[] = [
-  new Message("user", "Hello! How are you?"),
-  new Message("model", "I'm doing well, thank you! How about you?"),
-  new Message("user", "I'm good too. Just working on a project."),
-  new Message("model", "That sounds interesting. What kind of project is it?"),
-  new Message(
-    "user",
-    "It's a web development project using the latest frameworks."
-  ),
-  new Message("model", "That's great! Which frameworks are you using?"),
-  new Message(
-    "user",
-    "I'm using React.js for the frontend and Node.js for the backend."
-  ),
-  new Message(
-    "model",
-    "Excellent choice! Both React.js and Node.js are popular and powerful tools."
-  ),
-  new Message(
-    "user",
-    "Thank you! I'm excited about how the project is shaping up."
-  ),
-  new Message(
-    "model",
-    "I'm sure it will turn out great. Best of luck with your development!"
-  ),
-];
-
 export default function GptClient() {
   return (
     <div className={styles.body}>
       <Chats />
       <div className={styles.mainContent}>
         <Header />
-        <ChatFlow messages={testData} />
+        <ChatFlow />
       </div>
       <Settings />
     </div>
