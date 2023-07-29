474737e9cb2f498bc03355242d59b099ef4cd56c
eflisback <ebbe.flisback@gmail.com>
2023-07-29 20:14:19 +0200

    State variable control active page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,64 @@
+import { useState } from "react";
 import styles from "./Home.module.css";
 
 // Components
 import NavBar from "./NavBar/NavBar";
 
+interface IPage {
+  displayText: string;
+  id: string;
+  component?: JSX.Element;
+}
+
+const pages: IPage[] = [
+  {
+    displayText: "Welcome",
+    id: "welcome",
+  },
+  {
+    displayText: "Custom GPT Client",
+    id: "gpt",
+  },
+  {
+    displayText: "Test-page 1",
+    id: "t1",
+  },
+  {
+    displayText: "Test-page 2",
+    id: "t2",
+  },
+];
+
+const aboutPages: IPage[] = [
+  {
+    displayText: "General",
+    id: "general",
+  },
+  {
+    displayText: "Projects",
+    id: "projects",
+  },
+  {
+    displayText: "Experience",
+    id: "experience",
+  },
+  {
+    displayText: "Contact",
+    id: "contact",
+  },
+];
+
 export default function Home() {
+  const [activePageId, setActivePageId] = useState(pages[0].id);
+
   return (
     <div className={styles.body}>
-      <NavBar />
+      <NavBar
+        pages={pages}
+        aboutPages={aboutPages}
+        activePageId={activePageId}
+        setActivePageId={setActivePageId}
+      />
       <div className={styles.content}></div>
     </div>
   );
--- src/Pages/Home/NavBar/NavBar.tsx
+++ src/Pages/Home/NavBar/NavBar.tsx
@@ -1,16 +1,64 @@
 import styles from "./NavBar.module.css";
-import { Link } from "react-router-dom";
 
-export default function NavBar() {
+interface IPage {
+  displayText: string;
+  id: string;
+}
+
+interface IProps {
+  pages: IPage[];
+  aboutPages: IPage[];
+  activePageId: string;
+  setActivePageId: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function NavBar({
+  pages,
+  aboutPages,
+  activePageId,
+  setActivePageId,
+}: IProps) {
   return (
     <div className={styles.main}>
-      <span>ebbe.dev</span>
-      <div className={styles.links}>
-        <Link to="/gpt-client">Länk</Link>
-        <Link to={"/"}>Testlänk</Link>
-        <Link to={"/"}>Testlänk</Link>
-        <Link to={"/"}>Testlänk</Link>
-      </div>
+      <section>
+        <span className={styles.title}>ebbe.dev</span>
+      </section>
+      <section>
+        <div className={styles.subHeader}>Pages</div>
+        <div className={styles.links}>
+          {pages.map((page) => (
+            <button
+              type="button"
+              onClick={() => setActivePageId(page.id)}
+              className={page.id === activePageId ? styles.active : ""}
+            >
+              {page.displayText}
+            </button>
+          ))}
+        </div>
+      </section>
+      <section>
+        <div className={styles.subHeader}>About me</div>
+        <div className={styles.links}>
+          {aboutPages.map((page) => (
+            <button
+              type="button"
+              onClick={() => setActivePageId(page.id)}
+              className={page.id === activePageId ? styles.active : ""}
+            >
+              {page.displayText}
+            </button>
+          ))}
+        </div>
+      </section>
+      <section>
+        <div className={styles.subHeader}>Socials</div>
+        <div className={styles.socials}>
+          <a href="">LI</a>
+          <a href="">GH</a>
+          <a href="">IG</a>
+        </div>
+      </section>
     </div>
   );
 }
