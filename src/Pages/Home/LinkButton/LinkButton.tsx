09e96ed51b8a317f16d7bae608be64627634e007
eflisback <ebbe.flisback@gmail.com>
2023-07-29 21:48:52 +0200

    Progress on welcome page

---
+++ src/Pages/Home/LinkButton/LinkButton.tsx
@@ -0,0 +1,17 @@
+import styles from "./LinkButton.module.css";
+
+interface IProps {
+  title: string;
+  description: string;
+  icon: string;
+  link: string;
+}
+
+export default function LinkButton({ title, description, icon, link }: IProps) {
+  return (
+    <a href={link}>
+      <div className={styles.header}>{title}</div>
+      <div className={styles.content}>{description}</div>
+    </a>
+  );
+}
--- src/Pages/Welcome/Welcome.tsx
+++ src/Pages/Welcome/Welcome.tsx
@@ -1,9 +1,28 @@
 import styles from "./Welcome.module.css";
 
+// Components
+import LinkButton from "../Home/LinkButton/LinkButton";
+
 export default function Welcome() {
   return (
     <div className={styles.pageBody}>
-      <span>welcome</span>
+      <div className={styles.main}>
+        <span className={styles.title}>Welcome!</span>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+          quaerat voluptatibus enim impedit, beatae nihil hic dolorum soluta
+          porro amet, sequi necessitatibus doloribus blanditiis itaque
+          doloremque, in unde assumenda accusantium?
+        </p>
+        <div className={styles.linkButtonContainer}>
+          <LinkButton
+            title="Source repo"
+            description="The code of this website is open source and can be found here."
+            icon="someString"
+            link="nidoani"
+          />
+        </div>
+      </div>
     </div>
   );
 }
