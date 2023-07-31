3977492babd872b40941ac9f1f7d7489ad39d4e7
eflisback <ebbe.flisback@gmail.com>
2023-07-31 18:21:43 +0200

    404 page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import { useMemo, useState } from "react";
+import { useLocation } from "react-router-dom";
 import styles from "./Home.module.css";
 import { Routes, Route } from "react-router-dom";
 
@@ -9,6 +10,14 @@ import Header from "./Header/Header";
 // Pages
 import Welcome from "../Welcome/Welcome";
 import TestPage1 from "../TestPage1/TestPage1";
+
+import FourZeroFour from "./FourZeroFour/FourZeroFour";
+
+const fourZeroFour: IPage = {
+  displayText: "404 Not Found",
+  id: "404",
+  component: <FourZeroFour />,
+};
 
 const pages: IPage[] = [
   {
@@ -47,12 +56,25 @@ const aboutPages: IPage[] = [
 ];
 
 export default function Home() {
+  let location = useLocation();
   const [activePageId, setActivePageId] = useState(pages[0].id);
 
-  const activePage = useMemo(
-    () => [...pages, ...aboutPages].find((page) => page.id === activePageId),
-    [activePageId]
-  );
+  const activePage = useMemo(() => {
+    const pathId = location.pathname.slice(1);
+    if (pathId === "") {
+      setActivePageId(pages[0].id);
+      return pages[0];
+    }
+    const allPages = pages.concat(aboutPages);
+    const foundPage = allPages.find((page) => page.id === pathId);
+
+    if (foundPage) {
+      setActivePageId(foundPage.id);
+      return foundPage;
+    }
+
+    return fourZeroFour;
+  }, [location]);
 
   return (
     <div className={styles.body}>
@@ -66,10 +88,10 @@ export default function Home() {
         <Header displayText={activePage!.displayText} />
         <Routes>
           <Route path="/" element={<Welcome />} />
-          <Route path="/test" element={<TestPage1 />} />
+          {pages.concat(aboutPages).map((page) => (
+            <Route path={"/" + page.id} element={page.component} />
+          ))}
         </Routes>
-        {/*         {activePage!.component}
-         */}{" "}
       </div>
     </div>
   );
