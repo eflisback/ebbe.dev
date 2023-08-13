5b5450b4d966c4e4a575cbdbd156e55ce87aac92
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2023-08-13 22:37:09 +0200

    Margin/padding adjustments

--- src/Pages/Home/NavBar/NavBar.tsx
+++ src/Pages/Home/NavBar/NavBar.tsx
@@ -12,45 +12,47 @@ interface IProps {
 export default function NavBar({ pages, aboutPages, activePageId }: IProps) {
   return (
     <div className={styles.main}>
-      <section>
-        <div className={styles.title}>ebbe.dev</div>
-      </section>
-      <section>
-        <div className={styles.subHeader}>Pages</div>
-        <div className={styles.links}>
-          {pages.map((page) => (
-            <Link
-              key={page.id}
-              type="button"
-              to={"/" + page.id}
-              className={page.id === activePageId ? styles.active : ""}
-            >
-              <span className={styles.icon}>
-                <AiOutlineArrowRight />
-              </span>
-              <span>{page.displayText}</span>
-            </Link>
-          ))}
-        </div>
-      </section>
-      <section>
-        <div className={styles.subHeader}>About me</div>
-        <div className={styles.links}>
-          {aboutPages.map((page) => (
-            <Link
-              key={page.id}
-              type="button"
-              to={"/" + page.id}
-              className={page.id === activePageId ? styles.active : ""}
-            >
-              <span className={styles.icon}>
-                <AiOutlineArrowRight />
-              </span>
-              <span>{page.displayText}</span>
-            </Link>
-          ))}
-        </div>
-      </section>
+      <div>
+        <section>
+          <div className={styles.title}>ebbe.dev</div>
+        </section>
+        <section>
+          <div className={styles.subHeader}>Pages</div>
+          <div className={styles.links}>
+            {pages.map((page) => (
+              <Link
+                key={page.id}
+                type="button"
+                to={"/" + page.id}
+                className={page.id === activePageId ? styles.active : ""}
+              >
+                <span className={styles.icon}>
+                  <AiOutlineArrowRight />
+                </span>
+                <span>{page.displayText}</span>
+              </Link>
+            ))}
+          </div>
+        </section>
+        <section>
+          <div className={styles.subHeader}>About me</div>
+          <div className={styles.links}>
+            {aboutPages.map((page) => (
+              <Link
+                key={page.id}
+                type="button"
+                to={"/" + page.id}
+                className={page.id === activePageId ? styles.active : ""}
+              >
+                <span className={styles.icon}>
+                  <AiOutlineArrowRight />
+                </span>
+                <span>{page.displayText}</span>
+              </Link>
+            ))}
+          </div>
+        </section>
+      </div>
       <section>
         <div className={styles.subHeader}>Socials</div>
         <div className={styles.socials}>
