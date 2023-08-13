46c94e669a0c899b2c221755d2f2efdd5ce35746
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2023-08-14 00:18:59 +0200

    changed eslint config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,8 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:react-hooks/recommended",
+    /*     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+     */ "plugin:react-hooks/recommended",
   ],
   parser: "@typescript-eslint/parser",
   ignorePatterns: ["vite.config.ts", ".eslintrc.cjs"],
