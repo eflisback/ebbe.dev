0d42745c092b1a4b7c76e7bd6cae1447d8ae1a12
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2025-03-04 10:52:34 +0100

    Header refactor

--- src/components/overlay/start/Start.tsx
+++ src/components/overlay/start/Start.tsx
@@ -1,50 +1,13 @@
-import { ReactElement } from "react";
+import { Header } from "../header/Header";
 import styles from "./Start.module.scss";
-import { FaCode, FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
-
-interface SocialLink {
-  icon: ReactElement;
-  href: string;
-}
-
-const socialLinks: SocialLink[] = [
-  {
-    icon: <FaGithub />,
-    href: "https://github.com/eflisback/",
-  },
-  {
-    icon: <FaLinkedin />,
-    href: "https://www.linkedin.com/in/ebbe-flisb%C3%A4ck-a89a3a296/",
-  },
-  {
-    icon: <FaEnvelope />,
-    href: "mailto:ebbe.flisback@gmail.com",
-  },
-];
+import { FaCode } from "react-icons/fa";
 
 export const Start = () => {
   return (
     <div className={`${styles.overlay} ${styles.start}`}>
-      <div className={styles.header}>
-        <div className={styles.logo}>
-          <span>ebbe.</span>
-          <span className={styles.gradientText}>dev</span>
-        </div>
-        <div className={styles.socials}>
-          {socialLinks.map((link, index) => (
-            <a
-              key={index}
-              href={link.href}
-              className={styles.link}
-              target="_blank"
-            >
-              {link.icon}
-            </a>
-          ))}
-        </div>
-      </div>
+      <Header />
       <div className={styles.middleSection}>
-        <span></span>
+        
       </div>
       <div className={styles.footer}>
         <FaCode />
