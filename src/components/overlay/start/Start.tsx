ddd6eded0017b15400336650afeac84c8a72c582
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2025-03-04 19:06:41 +0100

    Basic CSS overflow handling

--- src/components/overlay/start/Start.tsx
+++ src/components/overlay/start/Start.tsx
@@ -51,6 +51,7 @@ export const Start = () => {
       <Header />
       <div className={styles.middleSection}>
         <div className={styles.title}>Projects</div>
+        <div className={styles.overflowContainer}>
         {projectLinks.map((link, index) => (
           <a
             key={index}
@@ -65,6 +66,7 @@ export const Start = () => {
             <span>{link.description}</span>
           </a>
         ))}
+        </div>
       </div>
       <div className={styles.footer}>
         <FaCode />
